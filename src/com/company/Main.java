e5b84b70d77cc85b1b147f03007fe6e8e46a7f37
Admin <kr77.kot@gmail.com>
2021-08-15 07:21:03 +0300

    Calc, на Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("Введите два числа: ");
+        int number1 = k.nextInt();
+        int number2 = k.nextInt();
+        System.out.print(number1 + number2);
+    }
+}
