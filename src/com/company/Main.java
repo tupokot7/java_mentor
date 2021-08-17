e18c2b22d6501d2c2d4df82a20d0c5d91a28693c
Admin <kr77.kot@gmail.com>
2021-08-17 06:33:49 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int value1;
-        int value2;
-        String operation;
 
         System.out.println("Input:");
         Scanner scanner = new Scanner(System.in);
-        value1 = scanner.nextInt();
-        operation = scanner.next();
-        value2 = scanner.nextInt();
-        if (operation.equals("+")) {
-            System.out.println(value1 + value2);
-        }
-        if (operation.equals("-")) {
-            System.out.println(value1 - value2);
-        }
-        if (operation.equals("*")) {
-            System.out.println("Output: " + "\n" + value1 * value2);
-        }
-        if (operation.equals("/")) {
-            System.out.println("Output: " + "\n" + value1 / value2);
+
+        int number1 = scanner.nextInt();
+        String operation = scanner.next();
+        int number2 = scanner.nextInt();
+
+        if (number1 <=10 && number2 <= 10) {
+            if (operation.equals("+")) {
+                System.out.println("Output: " + "\n" + (number1 + number2));
+            }
+            if (operation.equals("-")) {
+                System.out.println("Output: " + "\n" + (number1 - number2));
+            }
+            if (operation.equals("*")) {
+                System.out.println("Output: " + "\n" + (number1 * number2));
+            }
+            if (operation.equals("/")) {
+                System.out.println("Output: " + "\n" + (number1 / number2));
+            }
         }
     }
 }
