d9594630ffc786884821f803ebd2b25410b6f33b
Admin <kr77.kot@gmail.com>
2021-08-18 09:09:20 +0300

    conversion_1

---
+++ src/com/conversion/ConversionFromRomanToArabic.java
@@ -0,0 +1,46 @@
+package com.conversion;
+
+import java.util.Scanner;
+
+public class ConversionFromRomanToArabic {
+    static int value(char a) {
+        if (a =='m') return 1000;
+        else if (a =='d') return 500;
+        else if (a =='c') return 100;
+        else if (a =='l') return 50;
+        else if (a =='x') return 10;
+        else if (a =='v') return 5;
+        else if (a =='i') return 1;
+        else return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите римское число: ");
+        String roman = in.nextLine();
+        roman = roman.toLowerCase();
+        int val;
+        int val_next;
+        int temp;
+        int result = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+            val = value(roman.charAt(i));
+            if (i < roman.length() - 1) {
+                val_next = value(roman.charAt(i + 1));
+            } else val_next = 0;
+
+            if (val_next == 0) {
+                temp = val;
+            } else {
+                if (val > val_next) temp = val;
+                else if (val < val_next) {
+                    temp = val_next - val;
+                    i++;
+                } else temp = val;
+            }
+            result += temp;
+        }
+        System.out.println("Результат: " + result);
+    }
+}
